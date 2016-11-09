6028bfa27eda62a8f612223088b4ba4783bd46f4
andrewdudek84 <andrewdudek84@gmail.com>
2016-11-09 11:57:15 -0500

    Added new ws package

--- server.js
+++ server.js
@@ -1,53 +1,69 @@
-var http = require('http'),
-WebSocketServer = require('websocket').server,
-port = process.env.port || 3000,
-conn = [],
-UOW = require('./Modules/Shared/UOW'),
-Controller = require('./Modules/Controllers/Controller');
+var server = require('http').createServer()
+  , url = require('url')
+  , WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({ server: server })
+  , express = require('express')
+  , app = express()
+  , port = process.env.port || 3000
+  , UOW = require('./Modules/Shared/UOW')
+  , Controller = require('./Modules/Controllers/Controller')
+  , conn =[];
 
-var server = http.createServer(function (request, response) {
-    response.writeHead(404);
-    response.end();
+app.use(function (req, res) {
+  res.send({ msg: "hello" });
 });
 
-server.listen(port, function () {
-    console.log('Node server listening on port ' + port);
-});
+wss.on('connection', function connection(ws) {
+   var playerID = ws.upgradeReq.url.substring(1,ws.upgradeReq.url.length);
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    autoAcceptConnections: true
-});
-
-wsServer.on('close', function (request) {
-});
-
-wsServer.on('connect', function (connection) {
-    console.log('WebSocket Client Connected');
+   console.log('WebSocket Client Connected: ' + playerID);
     try {
-        conn.push(connection);
-        connection.on('message', function (req) {
+        ws["PlayerID"] = playerID;
+        conn.push(ws);
 
-            var request = JSON.parse(req.utf8Data);
+        ws.on('message', function incoming(message) {
+
+            var request = JSON.parse(message);
+
             console.log("Request Type: " + request.RequestType);
+           
+
             if (request.RequestType == "db") {
 
                 new Controller({ UOW: new UOW(function (args) {
-                   console.log("Return: " + JSON.stringify("{\"ResultType\":\"" + args.ResultType + "\",\"Results\":\""+JSON.stringify(args.Results)+"\"}")); 
+                
+                console.log("Return: " + JSON.stringify("{\"ResultType\":\"" + args.ResultType + "\",\"Results\":\""+JSON.stringify(args.Results)+"\"}")); 
+
+                 var rtnMessage = "{\"ResultType\":\"" + args.ResultType + "\",\"Results\":"+JSON.stringify(args.Results)+"}";
+                 console.log("Broadcast: " + request.Broadcast);
+                 
+                 if( request.Broadcast == "all"){
                     for (var c = 0; c < conn.length; c++) {
                         if (!conn[c].closed) {
-                            conn[c].sendUTF("{\"ResultType\":\"" + args.ResultType + "\",\"Results\":"+JSON.stringify(args.Results)+"}");
+                            conn[c].send(rtnMessage);
                         } else {
                             conn.splice(c, 1);
                         }
                     }
+                 }else{
+                     for (var c = 0; c < conn.length; c++) {
+                        if (!conn[c].closed) {
+                            if(conn[c].PlayerID === request.Broadcast){
+                                conn[c].send(rtnMessage);
+                            }
+                        } else {
+                            conn.splice(c, 1);
+                        }
+                    }
+                      
+                 }
                 }), Request: request.Request });
 
             } else {
 
                 for (var c = 0; c < conn.length; c++) {
                     if (!conn[c].closed) {
-                        conn[c].sendUTF(req.utf8Data);
+                        conn[c].send(message);
                     } else {
                         conn.splice(c, 1);
                     }
@@ -57,5 +73,17 @@ wsServer.on('connect', function (connection) {
     } catch (e) {
         console.log(e.message);
     }
+  // you might use location.query.access_token to authenticate or share sessions
+  // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
 });
+
+
+
+server.on('request', app);
+server.listen(port, function () { console.log('Listening on ' + server.address().port) });
 
